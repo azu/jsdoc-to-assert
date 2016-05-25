5147b4b0a15449225a67809c1bd9d4c74268fa2d
azu <azuciao@gmail.com>
2016-05-25 16:46:24 +0900

    feat(generator): use `new throw Error` pattern by default

--- src/CodeGenerator.js
+++ src/CodeGenerator.js
@@ -1,5 +1,12 @@
 // LICENSE : MIT
 "use strict";
+function trimSpaceEachLine(text) {
+    return text
+        .split("\n")
+        .filter(line => line != null)
+        .map(line => line.trim())
+        .join("\n");
+}
 export default class CodeGenerator {
     /**
      * @param commentTagNode commentTagNode is doctrine tag node
@@ -17,8 +24,8 @@ export default class CodeGenerator {
             throw new Error("should not contain ,");
         }
         // TODO: more safe using AST?
-        const trimmedExpression = expression.trim();
+        const trimmedExpression = trimSpaceEachLine(expression);
         const noSpaceExpression = trimmedExpression.replace(/\n/g, "\\n");
-        return `console.assert(${trimmedExpression},'${noSpaceExpression}');`;
+        return `if(${trimmedExpression}){ throw new TypeError('Invalid JSDoc @param: ${noSpaceExpression}'); }`;
     }
 }
