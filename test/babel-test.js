e401b9a06a8c8a5570d9ac29a5570a84f6da0aa1
azu <azuciao@gmail.com>
2016-05-25 16:50:15 +0900

    test(generator): fix test

--- test/babel-test.js
+++ test/babel-test.js
@@ -48,8 +48,13 @@ function myFunc(param, b, c){}
  * @param {string[]} [c] - this is a param.
  */
 function myFunc(param, b, c) {
-  console.assert(typeof param === \"number\", 'typeof param === \"number\"');
-  console.assert(typeof b === \"string\", 'typeof b === \"string\"');
+  if (typeof param === "number") {
+    throw new TypeError('Invalid JSDoc @param: typeof param === "number"');
+  }
+
+  if (typeof b === "string") {
+    throw new TypeError('Invalid JSDoc @param: typeof b === "string"');
+  }
 }`);
     });
 });
