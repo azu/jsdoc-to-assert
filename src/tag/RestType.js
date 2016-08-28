9a16a5f995d3ea48059e0e7395edcb6816044fdc
azu <azuciao@gmail.com>
2016-08-28 20:49:12 +0900

    feat(tag): support RestType
    ```
    /**
     * @param {...number} x - this is spread param.
     */
     ```
    
    to be
    
    ```js
    Array.isArray(x) && x.every(function (item) {
        return typeof item === 'number';
    });
    ```

--- src/tag/RestType.js
+++ src/tag/RestType.js
@@ -1,7 +1,41 @@
 // LICENSE : MIT
 "use strict";
-// @param {...} tag
- 
-export function RestType(tag, CodeGenerator) {
+import {Expression} from "./Expression";
+// @param {...number} tag
 
+/*
+ {
+  title: 'param',
+  description: 'this is spread param.',
+  type:
+   { type: 'RestType',
+     expression:
+     {
+        type: 'NameExpression', name: 'number'
+     }
+  },
+  name: 'x' }
+ */
+/**
+ * @param tag
+ * @param CodeGenerator
+ * @returns {undefined|string}
+ */
+export function RestType(tag, CodeGenerator) {
+    const expectedType = tag.type.expression.name;
+    if (tag.type.type !== "RestType") {
+        return;
+    }
+    const expression = tag.type.expression;
+    if (typeof expression === "object") {
+        const createGenericsAssert = () => {
+            // or
+            const expectedType = Expression("item", expression);
+            // Array.<String,Number>
+            // => (String || Number)
+            return `function(item){ return (${expectedType}); }`;
+        };
+        const expressionString = `${tag.name}.every(${createGenericsAssert()})`;
+        return CodeGenerator.assert(`Array.isArray(${tag.name}) && ${expressionString}`);
+    }
 }
